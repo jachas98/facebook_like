b61b91b54088153049c66f1a3c3975bec1314df7
Jachas <sc00r398@gmail.com>
2017-11-12 18:43:19 +0100

    Get db datas

--- social/views.py
+++ social/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from social.models import Post
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'social/post_list.html', {})
+    posts = Post.objects.filter().order_by('published_date')
+    return render(request, 'social/post_list.html', {'posts': posts})
